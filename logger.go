ff8d5a13941b19e1706dffbf1d2296f2a24a4405
jeppeter <jeppeter@gmail.com>
2016-02-25 11:22:43 +0800

    to make the logger ok for logging

--- logger.go
+++ logger.go
@@ -254,6 +254,155 @@ func (l *Logger) Debugf(format string, args ...interface{}) {
 	l.log(DEBUG, &format, args...)
 }
 
+var (
+	root_logger *Logger
+)
+
 func init() {
 	Reset()
+	root_logger = nil
+}
+
+// to replace logger
+func SetRootLogger(l *Logger) *Logger {
+	var retlogger *Logger
+	retlogger = root_logger
+	root_logger = l
+	return retlogger
+}
+
+// Fatal is equivalent to l.Critical(fmt.Sprint()) followed by a call to os.Exit(1).
+func Fatal(args ...interface{}) {
+	if root_logger == nil {
+		os.Exit(1)
+		return
+	}
+	root_logger.log(CRITICAL, nil, args...)
+	os.Exit(1)
+}
+
+// Fatalf is equivalent to l.Critical followed by a call to os.Exit(1).
+func Fatalf(format string, args ...interface{}) {
+	if root_logger == nil {
+		os.Exit(1)
+		return
+	}
+	root_logger.log(CRITICAL, &format, args...)
+	os.Exit(1)
+}
+
+// Panic is equivalent to l.Critical(fmt.Sprint()) followed by a call to panic().
+func Panic(args ...interface{}) {
+	if root_logger == nil {
+		panic(fmt.Sprint(args...))
+		return
+	}
+	root_logger.log(CRITICAL, nil, args...)
+	panic(fmt.Sprint(args...))
+}
+
+// Panicf is equivalent to l.Critical followed by a call to panic().
+func Panicf(format string, args ...interface{}) {
+	if root_logger == nil {
+		panic(fmt.Sprint(args...))
+		return
+	}
+	root_logger.log(CRITICAL, &format, args...)
+	panic(fmt.Sprintf(format, args...))
+}
+
+// Critical logs a message using CRITICAL as log level.
+func Critical(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(CRITICAL, nil, args...)
+}
+
+// Criticalf logs a message using CRITICAL as log level.
+func Criticalf(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(CRITICAL, &format, args...)
+}
+
+// Error logs a message using ERROR as log level.
+func Error(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(ERROR, nil, args...)
+}
+
+// Errorf logs a message using ERROR as log level.
+func Errorf(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(ERROR, &format, args...)
+}
+
+// Warning logs a message using WARNING as log level.
+func Warning(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(WARNING, nil, args...)
+}
+
+// Warningf logs a message using WARNING as log level.
+func Warningf(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(WARNING, &format, args...)
+}
+
+// Notice logs a message using NOTICE as log level.
+func Notice(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(NOTICE, nil, args...)
+}
+
+// Noticef logs a message using NOTICE as log level.
+func Noticef(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(NOTICE, &format, args...)
+}
+
+// Info logs a message using INFO as log level.
+func Info(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(INFO, nil, args...)
+}
+
+// Infof logs a message using INFO as log level.
+func Infof(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(INFO, &format, args...)
+}
+
+// Debug logs a message using DEBUG as log level.
+func Debug(args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(DEBUG, nil, args...)
+}
+
+// Debugf logs a message using DEBUG as log level.
+func Debugf(format string, args ...interface{}) {
+	if root_logger == nil {
+		return
+	}
+	root_logger.log(DEBUG, &format, args...)
 }
